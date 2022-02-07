f50d3f97f6625f8c9323b8aa49a8855fd8576bd7
petrostrak <pit.trak@gmail.com>
2022-02-07 11:55:00 +0200

    Add b flag to count the number of bytes

--- Basic-Word-Counter/main.go
+++ Basic-Word-Counter/main.go
@@ -11,25 +11,34 @@ import (
 // echo "My first command line tool with Go" | ./Basic-Word-Counter
 func main() {
 
-	// defineing a boolean flag -l to count lines instead of words.
+	// defining a boolean flag -l to count lines instead of words.
 	lines := flag.Bool("l", false, "Count lines")
+
+	// defining a boolean flag -b to count the total bytes read instead of words
+	bytesRead := flag.Bool("b", false, "Count total bytes read")
 
 	// parse the flags provided by the user.
 	flag.Parse()
 
 	// calling the count function to count the number of words (or lines)
 	// received from the stdIn.
-	fmt.Println(count(os.Stdin, *lines))
+	fmt.Println(count(os.Stdin, *lines, *bytesRead))
 }
 
-func count(r io.Reader, countLines bool) int {
+func count(r io.Reader, countLines, countBytes bool) int {
 
 	// a scanner is used to read text from a Reader.
 	scanner := bufio.NewScanner(r)
 
-	// if the count lines flag is not set, we want to count words
-	if !countLines {
-		// define the scanner split type to words (default is split by lines)
+	switch {
+	case countLines:
+		// if countLines flag is passed, we count lines
+		scanner.Split(bufio.ScanLines)
+	case countBytes:
+		// if countBytes is passed, we count bytes
+		scanner.Split(bufio.ScanBytes)
+	default:
+		// the default case is to count words
 		scanner.Split(bufio.ScanWords)
 	}
 
--- Basic-Word-Counter/main_test.go
+++ Basic-Word-Counter/main_test.go
@@ -9,7 +9,7 @@ func TestCountWords(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word 3word4\n")
 
 	expected := 4
-	response := count(b, false)
+	response := count(b, false, false)
 
 	if response != expected {
 		t.Errorf("Expected %d; got %d instead.", expected, response)
@@ -20,7 +20,18 @@ func TestCountLines(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3\nline2\nline3 word1")
 
 	expected := 3
-	response := count(b, true)
+	response := count(b, true, false)
+
+	if response != expected {
+		t.Errorf("Expected %d; got %d instead.", expected, response)
+	}
+}
+
+func TestCountBytes(t *testing.T) {
+	b := bytes.NewBufferString("My first command line tool with Go\n")
+
+	expected := 35
+	response := count(b, false, true)
 
 	if response != expected {
 		t.Errorf("Expected %d; got %d instead.", expected, response)
