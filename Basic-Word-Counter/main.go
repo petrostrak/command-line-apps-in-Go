5f3f59190156a12ac3770629bb2994722fa771e6
petrostrak <pit.trak@gmail.com>
2022-02-07 10:46:30 +0200

    Adding Command-Line Flags

--- Basic-Word-Counter/main.go
+++ Basic-Word-Counter/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
@@ -9,16 +10,28 @@ import (
 
 // echo "My first command line tool with Go" | ./Basic-Word-Counter
 func main() {
-	fmt.Println(count(os.Stdin))
+
+	// defineing a boolean flag -l to count lines instead of words.
+	lines := flag.Bool("l", false, "Count lines")
+
+	// parse the flags provided by the user.
+	flag.Parse()
+
+	// calling the count function to count the number of words (or lines)
+	// received from the stdIn.
+	fmt.Println(count(os.Stdin, *lines))
 }
 
-func count(r io.Reader) int {
+func count(r io.Reader, countLines bool) int {
 
 	// a scanner is used to read text from a Reader.
 	scanner := bufio.NewScanner(r)
 
-	// define the scanner split type to words (default is split by lines)
-	scanner.Split(bufio.ScanWords)
+	// if the count lines flag is not set, we want to count words
+	if !countLines {
+		// define the scanner split type to words (default is split by lines)
+		scanner.Split(bufio.ScanWords)
+	}
 
 	// define the counter
 	wc := 0
--- Basic-Word-Counter/main_test.go
+++ Basic-Word-Counter/main_test.go
@@ -9,7 +9,18 @@ func TestCountWords(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word 3word4\n")
 
 	expected := 4
-	response := count(b)
+	response := count(b, false)
+
+	if response != expected {
+		t.Errorf("Expected %d; got %d instead.", expected, response)
+	}
+}
+
+func TestCountLines(t *testing.T) {
+	b := bytes.NewBufferString("word1 word2 word3\nline2\nline3 word1")
+
+	expected := 3
+	response := count(b, true)
 
 	if response != expected {
 		t.Errorf("Expected %d; got %d instead.", expected, response)
