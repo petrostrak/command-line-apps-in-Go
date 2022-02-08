e9e7ca55d9f6415329cfd3fb1a7b3b7395d61efa
petrostrak <pit.trak@gmail.com>
2022-02-08 21:17:44 +0200

    Add test delete

--- Todo/todo_test.go
+++ Todo/todo_test.go
@@ -23,6 +23,7 @@ func TestAdd(t *testing.T) {
 	}
 }
 
+// TestComplete tests the Complete method of the List type
 func TestComplete(t *testing.T) {
 	l := List{}
 
@@ -47,5 +48,34 @@ func TestComplete(t *testing.T) {
 
 	if !l[0].Done {
 		t.Errorf("New task should be completed")
+	}
+}
+
+// TestDelete tests the Delete method of the List type
+func TestDelete(t *testing.T) {
+	l := List{}
+
+	tasks := []Item{
+		{"New task 1", true, time.Now(), time.Now()},
+		{"New task 2", false, time.Now(), time.Time{}},
+		{"New task 3", false, time.Now(), time.Time{}},
+	}
+
+	for _, i := range tasks {
+		l.Add(i)
+	}
+
+	if l[0].Task != tasks[0].Task {
+		t.Errorf("expected %q, got %q", tasks[0].Task, l[0].Task)
+	}
+
+	l.Delete(2)
+
+	if len(l) != 2 {
+		t.Errorf("expected list length %d, got %d instead", 2, len(l))
+	}
+
+	if l[1].Task != tasks[2].Task {
+		t.Errorf("expected %q, got %q instead", tasks[2].Task, l[1].Task)
 	}
 }
