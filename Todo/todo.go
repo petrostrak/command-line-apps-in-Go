5f6fe81dd51770e34273cd1a020f8e4881acaf7f
petrostrak <pit.trak@gmail.com>
2022-02-08 09:41:28 +0200

    Add item and list struct

--- Todo/todo.go
+++ Todo/todo.go
@@ -1,1 +1,14 @@
 package todo
+
+import "time"
+
+// item struct represents a ToDo item
+type Item struct {
+	Task        string
+	Done        bool
+	CreatedAt   time.Time
+	CompletedAt time.Time
+}
+
+// list represents a list of DoTo items
+type List []Item
