0d3096d4a6c327786f3fca13c6455428c62b28a2
petrostrak <pit.trak@gmail.com>
2022-02-09 21:27:07 +0200

    Capturing Input from STDIN

--- Todo/cmd/todo/main.go
+++ Todo/cmd/todo/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 	"time"
 
 	todo "github.com/petrostrak/command-line-apps-in-Go/Todo"
@@ -32,7 +36,7 @@ func main() {
 	}
 
 	// parsing command line flags
-	task := flag.String("task", "", "Task to be included in the todo list")
+	add := flag.Bool("add", false, "Add task to the todo list")
 	list := flag.Bool("list", false, "List all tasks")
 	complete := flag.Int("complete", 0, "Item to be completed")
 
@@ -71,11 +75,19 @@ func main() {
 			os.Exit(1)
 		}
 
-	case *task != "":
-		task := todo.Item{*task, false, time.Now(), time.Time{}}
+	case *add:
+		// when any arguments (excluding flags) are provided, they will be
+		// used as the new task
+		t, err := getTask(os.Stdin, flag.Args()...)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		task := todo.Item{t, false, time.Now(), time.Time{}}
 		// add the task
 		l.Add(task)
 
+		// save the new list
 		if err := l.Save(todoFileName); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
@@ -88,4 +100,24 @@ func main() {
 
 	}
 
+}
+
+// getTask function decides where to get the description for a new
+// task from: arguments or STDIN
+func getTask(r io.Reader, args ...string) (string, error) {
+	if len(os.Args) > 0 {
+		return strings.Join(args, " "), nil
+	}
+
+	s := bufio.NewScanner(r)
+	s.Scan()
+	if err := s.Err(); err != nil {
+		return "", err
+	}
+
+	if len(s.Text()) == 0 {
+		return "", errors.New("task cannot be blank")
+	}
+
+	return s.Text(), nil
 }
