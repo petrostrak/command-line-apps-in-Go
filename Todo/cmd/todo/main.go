083ae44a1c36e7bada70906d93a1962a91655160
petrostrak <pit.trak@gmail.com>
2022-02-10 13:44:02 +0200

    Add delete flag and functionality

--- Todo/cmd/todo/main.go
+++ Todo/cmd/todo/main.go
@@ -39,6 +39,7 @@ func main() {
 	add := flag.Bool("add", false, "Add task to the todo list")
 	list := flag.Bool("list", false, "List all tasks")
 	complete := flag.Int("complete", 0, "Item to be completed")
+	delete := flag.Int("delete", 0, "Item to be deleted")
 
 	flag.Parse()
 
@@ -66,6 +67,18 @@ func main() {
 	case *complete > 0:
 		// complete the given item
 		if err := l.Complete(*complete); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+	case *delete > 0:
+		// delete the given item
+		if err := l.Delete(*delete); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
