3cf2ccc6d17fcf698a9123ad91cc5573650baf5b
petrostrak <pit.trak@gmail.com>
2022-02-22 09:58:54 +0200

    Add exceptionStep execute method

--- CI/exceptionStep.go
+++ CI/exceptionStep.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+)
+
 type exceptionStep struct {
 	step
 }
@@ -10,4 +16,31 @@ func newExceptionStep(name, exe, msg, proj string, args []string) exceptionStep 
 	s.step = newStep(name, exe, msg, proj, args)
 
 	return s
+}
+
+func (s exceptionStep) execute() (string, error) {
+	cmd := exec.Command(s.exe, s.args...)
+
+	var out bytes.Buffer
+
+	cmd.Stdout = &out
+	cmd.Dir = s.proj
+
+	if err := cmd.Run(); err != nil {
+		return "", &stepErr{
+			step:  s.name,
+			msg:   "failed to execute",
+			cause: err,
+		}
+	}
+
+	if out.Len() > 0 {
+		return "", &stepErr{
+			step:  s.name,
+			msg:   fmt.Sprintf("invalid format: %s", out.String()),
+			cause: nil,
+		}
+	}
+
+	return s.message, nil
 }
