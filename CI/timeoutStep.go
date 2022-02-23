da39ee278f8c0c29cce6a54e51d71f99f4bda91c
petrostrak <pit.trak@gmail.com>
2022-02-23 10:36:07 +0200

    Add timeoutStep execute method

--- CI/timeoutStep.go
+++ CI/timeoutStep.go
@@ -1,6 +1,10 @@
 package main
 
-import "time"
+import (
+	"context"
+	"os/exec"
+	"time"
+)
 
 type timeoutStep struct {
 	step
@@ -18,4 +22,32 @@ func newTimeoutStep(name, exe, msg, proj string, args []string, timeout time.Dur
 	}
 
 	return s
+}
+
+func (s timeoutStep) execute() (string, error) {
+
+	// define a context to carry the timeout value
+	ctx, cancel := context.WithTimeout(context.Background(), s.timeout)
+	defer cancel()
+
+	cmd := exec.CommandContext(ctx, s.exe, s.args...)
+	cmd.Dir = s.proj
+
+	if err := cmd.Run(); err != nil {
+		if ctx.Err() == context.DeadlineExceeded {
+			return "", &stepErr{
+				step:  s.name,
+				msg:   "failed time out",
+				cause: context.DeadlineExceeded,
+			}
+		}
+
+		return "", &stepErr{
+			step:  s.name,
+			msg:   "failed to execute",
+			cause: err,
+		}
+	}
+
+	return s.message, nil
 }
