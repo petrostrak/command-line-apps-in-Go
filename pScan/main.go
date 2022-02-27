f1fd7625d4f62734f5b95f5ef7be06799aca50bc
petrostrak <pit.trak@gmail.com>
2022-02-27 12:08:47 +0200

    Add the first Subcommand hosts

--- pScan/main.go
+++ pScan/main.go
@@ -14,6 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 cobra init --pkg-name github.com/petrostrak/command-line-apps-in-Go/pScan
+
+To export path
+export PATH=$(go env GOPATH)/bin:$PATH
 */
 package main
 
