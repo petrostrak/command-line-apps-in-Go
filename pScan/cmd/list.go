c57f966fee3975efef0dcab0139023133dcc7158
petrostrak <pit.trak@gmail.com>
2022-02-28 09:39:21 +0200

    Change boilerplate code in list

--- pScan/cmd/list.go
+++ pScan/cmd/list.go
@@ -17,6 +17,7 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -24,15 +25,18 @@ import (
 // listCmd represents the list command
 var listCmd = &cobra.Command{
 	Use:   "list",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "List hosts in hosts list",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("list called")
+	},
+	Aliases: []string{"l"},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		hostsFile, err := cmd.Flags().GetString("hosts-file")
+		if err != nil {
+			return err
+		}
+
+		return listAction(os.Stdout, hostsFile, args)
 	},
 }
 
