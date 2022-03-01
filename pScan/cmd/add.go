80eea6f1330c935738a63dc782a123db7cd9e77e
petrostrak <pit.trak@gmail.com>
2022-03-01 11:19:32 +0200

    Add addAction

--- pScan/cmd/add.go
+++ pScan/cmd/add.go
@@ -17,7 +17,10 @@ package cmd
 
 import (
 	"fmt"
+	"io"
+	"os"
 
+	"github.com/petrostrak/command-line-apps-in-Go/pScan/scan"
 	"github.com/spf13/cobra"
 )
 
@@ -25,8 +28,13 @@ import (
 var addCmd = &cobra.Command{
 	Use:   "add <host1>...<hostn>",
 	Short: "Add new host(s) to list",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("add called")
+	RunE: func(cmd *cobra.Command, args []string) error {
+		hostsFile, err := cmd.Flags().GetString("hosts-file")
+		if err != nil {
+			return err
+		}
+
+		return addAction(os.Stdout, hostsFile, args)
 	},
 	Aliases:      []string{"a"},
 	Args:         cobra.MinimumNArgs(1),
@@ -45,4 +53,22 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func addAction(out io.Writer, hostsFile string, args []string) error {
+	hl := &scan.HostsList{}
+
+	if err := hl.Load(hostsFile); err != nil {
+		return err
+	}
+
+	for _, h := range args {
+		if err := hl.Add(h); err != nil {
+			return err
+		}
+
+		fmt.Fprintln(out, "Added host:", h)
+	}
+
+	return hl.Save(hostsFile)
 }
