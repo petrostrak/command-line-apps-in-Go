cbf69bba2066f280deeeabcd507361d9df5b723b
petrostrak <pit.trak@gmail.com>
2022-03-01 15:31:53 +0200

    Add TestHostActions

--- pScan/cmd/actions_test.go
+++ pScan/cmd/actions_test.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"bytes"
+	"io"
 	"io/ioutil"
 	"os"
 	"testing"
@@ -32,5 +34,65 @@ func setup(t *testing.T, hosts []string, initList bool) (string, func()) {
 	// Return temp file name and cleanup function
 	return tf.Name(), func() {
 		os.Remove(tf.Name())
+	}
+}
+
+func TestHostActions(t *testing.T) {
+	// define hosts for actions test
+	hosts := []string{
+		"host1",
+		"host2",
+		"host3",
+	}
+
+	// test cases for action test (table-driven approach)
+	testCases := []struct {
+		name           string
+		args           []string
+		expectedOut    string
+		initList       bool
+		actionFunction func(io.Writer, string, []string) error
+	}{
+		{
+			name:           "AddAction",
+			args:           hosts,
+			expectedOut:    "Added host: host1\nAdded host: host2\nAdded host: host3\n",
+			initList:       false,
+			actionFunction: addAction,
+		},
+		{
+			name:           "ListAction",
+			expectedOut:    "host1\nhost2\nhost3\n",
+			initList:       true,
+			actionFunction: listAction,
+		},
+		{
+			name:           "DeleteAction",
+			args:           []string{"host1", "host2"},
+			expectedOut:    "Deleted host: host1\nDeleted host: host2\n",
+			initList:       true,
+			actionFunction: deleteAction,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			// setup action test
+			tf, cleanup := setup(t, hosts, tc.initList)
+			defer cleanup()
+
+			// define var to capture action output
+			var out bytes.Buffer
+
+			// execute action and capture output
+			if err := tc.actionFunction(&out, tf, tc.args); err != nil {
+				t.Fatalf("expected no error, got %q\n", err)
+			}
+
+			// test actions output
+			if out.String() != tc.expectedOut {
+				t.Errorf("expected output %q, got %q\n", tc.expectedOut, out.String())
+			}
+		})
 	}
 }
