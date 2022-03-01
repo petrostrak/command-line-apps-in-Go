aee75d8b5057c78156641bed71fc443ef56b05d4
petrostrak <pit.trak@gmail.com>
2022-03-01 11:36:34 +0200

    Add deleteAction

--- pScan/cmd/delete.go
+++ pScan/cmd/delete.go
@@ -17,22 +17,27 @@ package cmd
 
 import (
 	"fmt"
+	"io"
+	"os"
 
+	"github.com/petrostrak/command-line-apps-in-Go/pScan/scan"
 	"github.com/spf13/cobra"
 )
 
 // deleteCmd represents the delete command
 var deleteCmd = &cobra.Command{
-	Use:   "delete",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Use:          "delete <host1>...<host n>",
+	Aliases:      []string{"d"},
+	Short:        "Delete hosts(s) from list",
+	SilenceUsage: true,
+	Args:         cobra.MaximumNArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		hostsFile, err := cmd.Flags().GetString("hosts-file")
+		if err != nil {
+			return err
+		}
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("delete called")
+		return deleteAction(os.Stdout, hostsFile, args)
 	},
 }
 
@@ -48,4 +53,21 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// deleteCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func deleteAction(out io.Writer, hostsFile string, args []string) error {
+	hl := &scan.HostsList{}
+
+	if err := hl.Load(hostsFile); err != nil {
+		return err
+	}
+
+	for _, h := range args {
+		if err := hl.Remove(h); err != nil {
+			return err
+		}
+		fmt.Fprintln(out, "Deleted host:", h)
+	}
+
+	return hl.Save(hostsFile)
 }
