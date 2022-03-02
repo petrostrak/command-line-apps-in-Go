cec93baac8ff11281e2adf18fa0de96e1f6fad02
petrostrak <pit.trak@gmail.com>
2022-03-02 22:55:46 +0200

    Add TestDelete

--- TodoServer/server_test.go
+++ TodoServer/server_test.go
@@ -189,3 +189,56 @@ func TestAdd(t *testing.T) {
 		}
 	})
 }
+
+func TestDelete(t *testing.T) {
+	url, cleanup := setupAPI(t)
+	defer cleanup()
+
+	t.Run("Delete", func(t *testing.T) {
+		u := fmt.Sprintf("%s/todo/1", url)
+
+		req, err := http.NewRequest(http.MethodDelete, u, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		r, err := http.DefaultClient.Do(req)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if r.StatusCode != http.StatusNoContent {
+			t.Fatalf("Expected %q, got %q.",
+				http.StatusText(http.StatusNoContent),
+				http.StatusText(r.StatusCode))
+		}
+	})
+	t.Run("CheckDelete", func(t *testing.T) {
+		r, err := http.Get(url + "/todo")
+		if err != nil {
+			t.Error(err)
+		}
+
+		if r.StatusCode != http.StatusOK {
+			t.Fatalf("Expected %q, got %q.",
+				http.StatusText(http.StatusOK), http.StatusText(r.StatusCode))
+		}
+
+		var resp todoResponse
+
+		if err := json.NewDecoder(r.Body).Decode(&resp); err != nil {
+			t.Fatal(err)
+		}
+		r.Body.Close()
+
+		if len(resp.Results) != 1 {
+			t.Errorf("Expected 1 item, got %d.", len(resp.Results))
+		}
+
+		expTask := "Task number 2."
+
+		if resp.Results[0].Task != expTask {
+			t.Errorf("Expected %q, got %q.", expTask, resp.Results[0].Task)
+		}
+	})
+}
