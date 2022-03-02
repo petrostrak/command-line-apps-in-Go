40ed38e1014c133307da5f3d207be67ae97684c5
petrostrak <pit.trak@gmail.com>
2022-03-02 14:19:21 +0200

    Add replyTextContent

--- TodoServer/handlers.go
+++ TodoServer/handlers.go
@@ -11,3 +11,9 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	content := "There's an API here"
 	replyTextContent(w, r, http.StatusOK, content)
 }
+
+func replyTextContent(w http.ResponseWriter, r *http.Request, status int, content string) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(status)
+	w.Write([]byte(content))
+}
