92e8781e7a6397f860175dce6be7f4fdb4283234
petrostrak <pit.trak@gmail.com>
2022-03-02 14:51:43 +0200

    Add TestGet

--- TodoServer/server_test.go
+++ TodoServer/server_test.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"io/ioutil"
+	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -12,5 +15,59 @@ func setupAPI(t *testing.T) (string, func()) {
 
 	return ts.URL, func() {
 		ts.Close()
+	}
+}
+
+func TestGet(t *testing.T) {
+	testCases := []struct {
+		name       string
+		path       string
+		expCode    int
+		expItems   int
+		expContent string
+	}{
+		{name: "GetRoot", path: "/",
+			expCode:    http.StatusOK,
+			expContent: "There's an API here",
+		},
+		{name: "NotFound", path: "/todo/500",
+			expCode: http.StatusNotFound,
+		},
+	}
+
+	url, cleanUp := setupAPI(t)
+	defer cleanUp()
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			var (
+				body []byte
+				err  error
+			)
+
+			r, err := http.Get(url + tc.path)
+			if err != nil {
+				t.Error(err)
+			}
+			defer r.Body.Close()
+
+			if r.StatusCode != tc.expCode {
+				t.Fatalf("expected %q, got %q.", http.StatusText(tc.expCode), http.StatusText(r.StatusCode))
+			}
+
+			switch {
+			case strings.Contains(r.Header.Get("Content-Type"), "text/plain"):
+				if body, err = ioutil.ReadAll(r.Body); err != nil {
+					t.Error(err)
+				}
+
+				if !strings.Contains(string(body), tc.expContent) {
+					t.Errorf("Expected %q, got %q.", tc.expContent,
+						string(body))
+				}
+			default:
+				t.Fatalf("Unsupported Content-Type: %q", r.Header.Get("Content-Type"))
+			}
+		})
 	}
 }
