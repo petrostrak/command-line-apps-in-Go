a2b506524b6e6be4fc065e91a224a8540a1739e0
petrostrak <pit.trak@gmail.com>
2022-03-02 17:31:39 +0200

    Add replyJSONContent helper func

--- TodoServer/server.go
+++ TodoServer/server.go
@@ -1,9 +1,24 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 func newMux(todoFile string) http.Handler {
 	m := http.NewServeMux()
 	m.HandleFunc("/", rootHandler)
 	return m
+}
+
+func replyJSONContent(w http.ResponseWriter, r *http.Request, status int, resp *todoResponse) {
+	body, err := json.Marshal(resp)
+	if err != nil {
+		replyError(w, r, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(body)
 }
