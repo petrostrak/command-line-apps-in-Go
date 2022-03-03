2ed4f2485160f2d7762e89221173ab7d76ca32be
petrostrak <pit.trak@gmail.com>
2022-03-03 23:57:47 +0200

    Add printAll

--- Api/TodoClient/cmd/list.go
+++ Api/TodoClient/cmd/list.go
@@ -16,8 +16,10 @@ limitations under the License.
 package cmd
 
 import (
+	"fmt"
 	"io"
 	"os"
+	"text/tabwriter"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -55,4 +57,18 @@ func listAction(out io.Writer, apiRoot string) error {
 	}
 
 	return printAll(out, items)
+}
+
+func printAll(out io.Writer, items []item) error {
+	w := tabwriter.NewWriter(out, 3, 2, 0, ' ', 0)
+
+	for k, v := range items {
+		done := "-"
+		if v.Done {
+			done = "X"
+		}
+		fmt.Fprintf(w, "%s\t%d\t%s\t\n", done, k+1, v.Task)
+	}
+
+	return w.Flush()
 }
