0fbe75809fa3a8fb8a66ed5d33a007af2a11dc0b
petrostrak <pit.trak@gmail.com>
2022-03-04 00:11:25 +0200

    Add printOne

--- Api/TodoClient/cmd/view.go
+++ Api/TodoClient/cmd/view.go
@@ -20,6 +20,7 @@ import (
 	"io"
 	"os"
 	"strconv"
+	"text/tabwriter"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -63,4 +64,22 @@ func viewAction(out io.Writer, apiRoot, arg string) error {
 	}
 
 	return printOne(out, i)
+}
+
+func printOne(out io.Writer, i item) error {
+	w := tabwriter.NewWriter(out, 14, 2, 0, ' ', 0)
+
+	fmt.Fprintf(w, "Task:\t%s\n", i.Task)
+
+	fmt.Fprintf(w, "Created at:\t%s\n", i.CreatedAt.Format(timeFormat))
+
+	if i.Done {
+		fmt.Fprintf(w, "Completed:\t%s\n", "Yes")
+		fmt.Fprintf(w, "Completed At:\t%s\n", i.CompletedAt.Format(timeFormat))
+		return w.Flush()
+	}
+
+	fmt.Fprintf(w, "Completed:\t%s\n", "No")
+
+	return w.Flush()
 }
