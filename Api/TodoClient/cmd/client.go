9b3d742dfcd80c6bf284e707d0dd5d9f6a4e9f96
petrostrak <pit.trak@gmail.com>
2022-03-03 17:53:45 +0200

    Add getItems

--- Api/TodoClient/cmd/client.go
+++ Api/TodoClient/cmd/client.go
@@ -1,7 +1,10 @@
 package cmd
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"time"
 )
@@ -31,4 +34,37 @@ func newClient() *http.Client {
 	return &http.Client{
 		Timeout: 10 * time.Second,
 	}
+}
+
+func getItems(url string) ([]item, error) {
+	r, err := newClient().Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("%w: %s", ErrConnection, err)
+	}
+	defer r.Body.Close()
+
+	if r.StatusCode != http.StatusOK {
+		msg, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			return nil, fmt.Errorf("Cannot read body: %w", err)
+		}
+
+		err = ErrInvalidResponse
+		if r.StatusCode == http.StatusNotFound {
+			err = ErrNotFound
+		}
+		return nil, fmt.Errorf("%w: %s", err, msg)
+	}
+
+	var resp response
+
+	if err := json.NewDecoder(r.Body).Decode(&resp); err != nil {
+		return nil, err
+	}
+
+	if resp.TotalResults == 0 {
+		return nil, fmt.Errorf("%w: No results found", ErrNotFound)
+	}
+
+	return resp.Results, nil
 }
